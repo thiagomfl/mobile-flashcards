1ef9bb0ccc58515e6d347398fd0d78ebea488bd4
Thiago Moura <thmoura14@hotmail.com>
2019-01-02 13:23:14 -0300

    Deck Component done

--- components/Deck.js
+++ components/Deck.js
@@ -14,10 +14,62 @@ class Deck extends Component {
 
     return (
       <View style={styles.container}>
-        <Text> textInComponent </Text>
+        <View style={styles.header}>
+          <Text style={styles.name}>{deck.name}</Text>
+          <Text style={styles.count}>{`${deck.cards.length} ${pluralize(
+            "card",
+            deck.cards.length
+          )}`}</Text>
+        </View>
+        <View style={styles.actions}>
+          {deck.cards.length !== 0 && (
+            <Button
+              onPress={() => {
+                navigation.navigate("Quiz", { deck });
+              }}
+            >
+              <Text>Start Quiz</Text>
+            </Button>
+          )}
+          <Button
+            style={{ backgroundColor: deck.cards.length !== 0 ? gray : green }}
+            onPress={() => {
+              navigation.navigate("AddCard", { deckId: deck.id });
+            }}
+          >
+            <Text>Add Card</Text>
+          </Button>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 10,
+    marginBottom: 5
+  },
+  count: {
+    fontSize: 20,
+    color: gray,
+    textAlign: "center",
+    marginBottom: 5
+  },
+  actions: {
+    marginTop: 20
+  }
+});
+
+//mapDispatchToProps
 
 export default Deck;
