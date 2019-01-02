b3f41abdce9d4738cf4b943673b14985f8234d2b
Thiago Moura <thmoura14@hotmail.com>
2019-01-02 12:55:56 -0300

    Button Component Done

---
+++ components/Button.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { green, white } from "../utils/colors";
+
+const Button = ({ children, onPress, style = {} }) => (
+  <TouchableOpacity style={[style.button.style]} onPress={onPress}>
+    <Text style={[style.reset, style]}>{children}</Text>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 5,
+    backgroundColor: green,
+    margin: 10,
+    padding: 15,
+    width: 300
+  },
+  reset: {
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+    color: white
+  }
+});
+
+export default StyledButton;
